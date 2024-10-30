084e3526e5050987861fb899ccd8694d186134ad
Riichan04 <22130274@st.hcmuaf.edu.vn>
2024-10-30 22:56:17 +0700

    Update process for product page

---
+++ script/product.js
@@ -0,0 +1,16 @@
+function displayRating(rating, starWidth) {
+    const starPosition = Math.trunc(rating)
+    const starPositionWidthRating = rating - Math.floor(rating)
+    const starMaskWidth = starWidth * (1 - starPositionWidthRating)
+    const starPadding = ($(".product-info__star-container").width() - starWidth)/2
+    $(".product-info__star-mask").each(function (index) {
+        if (index === starPosition -1) {
+            $(this).css("width", starMaskWidth + starPadding + "px")
+        }
+        if (index > starPosition -1) {
+            $(this).css("width", "100%")
+        }
+    });
+}
+
+displayRating(3.6, 31.5)
