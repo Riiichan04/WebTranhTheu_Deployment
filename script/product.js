9147e97f3b16a73b5cbd119aa3608c421a46726e
Riichan04 <22130274@st.hcmuaf.edu.vn>
2024-11-03 16:54:59 +0700

    Hiển thị số sao của bình luận

--- script/product.js
+++ script/product.js
@@ -2,8 +2,8 @@ function displayRating(rating, starWidth) {
     const starPosition = Math.trunc(rating)
     const starPositionWidthRating = rating - Math.floor(rating)
     const starMaskWidth = starWidth * (1 - starPositionWidthRating)
-    const starPadding = ($(".product-info__star-container").width() - starWidth)/2
-    $(".product-info__star-mask").each(function (index) {
+    const starPadding = ($("#product-info__rating .product-info__star-container").width() - starWidth)/2
+    $("#product-info__rating .product-info__star-mask").each(function (index) {
         if (index === starPosition -1) {
             $(this).css("width", starMaskWidth + starPadding + "px")
         }
