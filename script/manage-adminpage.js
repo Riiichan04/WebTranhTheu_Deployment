97ce4cfba5e571ab92917787fca467e8b8a93070
Tran-3110 <22130276@st.hcmuaf.edu.vn>
2024-11-03 18:09:07 +0700

    add content of user management page and product management page

---
+++ script/manage-adminpage.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+    $('#myTable').DataTable( {
+        scrollY: "480px"
+    }); // Khởi tạo DataTable
+    $('.dt-search label').text("Tìm kiếm: ");
+    $('.dt-search label').css("margin-right", "10px");
+    $('.dt-length label').text("Số lượng hiển thị mỗi trang");
+    $('.dt-length label').css("margin-left", "10px");
+    $('.dt-info').css("display", "none");
+    $('.dt-search input').css("width", "300px");
+    $('table.dataTable th.dt-type-numeric').css("text-align", "center");
+});
