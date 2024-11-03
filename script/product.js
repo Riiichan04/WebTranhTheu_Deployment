b7a28686c0ac6fa7a22a416a6b021722c7ee1e6f
Riichan04 <22130274@st.hcmuaf.edu.vn>
2024-11-03 17:40:27 +0700

    Làm phần cửa sổ chọn địa chỉ giao hàng

--- script/product.js
+++ script/product.js
@@ -2,15 +2,23 @@ function displayRating(rating, starWidth) {
     const starPosition = Math.trunc(rating)
     const starPositionWidthRating = rating - Math.floor(rating)
     const starMaskWidth = starWidth * (1 - starPositionWidthRating)
-    const starPadding = ($("#product-info__rating .product-info__star-container").width() - starWidth)/2
+    const starPadding = ($("#product-info__rating .product-info__star-container").width() - starWidth) / 2
     $("#product-info__rating .product-info__star-mask").each(function (index) {
-        if (index === starPosition -1) {
+        if (index === starPosition - 1) {
             $(this).css("width", starMaskWidth + starPadding + "px")
         }
-        if (index > starPosition -1) {
+        if (index > starPosition - 1) {
             $(this).css("width", "100%")
         }
     });
 }
+
+$("#shipping-location__close-popup").click(function () {
+    return $("#shipping-location-container").css("display", "none");
+})
+
+$("#show-popup").click(function () {
+    return $("#shipping-location-container").css("display", "block");
+})
 
 displayRating(3.6, 31.5)
