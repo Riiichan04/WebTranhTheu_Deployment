f82fef2c1cff0c80cd5252260a19412f942e6b13
Tran-3110 <22130276@st.hcmuaf.edu.vn>
2024-11-12 01:10:35 +0700

    chỉnh sửa phân trang

--- script/search-result-pagination.js
+++ script/search-result-pagination.js
@@ -1,5 +1,5 @@
 // Tạo dữ liệu sản phẩm mẫu
-const products = Array.from({ length: 50 }, (_, i) => ({
+const products = Array.from({ length: 1000 }, (_, i) => ({
     url: 'https://www.thegioitranhtheu.com/media/catalog/product/cache/1/small_image/9df78eab33525d08d6e5fb8d27136e95/f/i/file_7_35.jpg',
     name: `Tranh thêu sông nước`,
     star: ['&#9733;', '&#9733;', '&#9733;', '&#9733;', '&#9733;'],
@@ -7,7 +7,7 @@ const products = Array.from({ length: 50 }, (_, i) => ({
 }));
 
 const productsPerPage = 20;
-let currentPage = 1;
+const totalPages = Math.ceil(products.length / productsPerPage);
 function displayProducts(page) {
     const productList = document.getElementById("productList");
     productList.innerHTML = ""; //xoa san pham cu
@@ -44,28 +44,53 @@ function displayProducts(page) {
     });
 }
 
-function setupPagination() {
-    const pagination = document.getElementById('pagination');
-    const totalPages = Math.ceil(products.length / productsPerPage);
-
-    pagination.innerHTML = "";
-
-    for(let i = 1; i <= totalPages; i++) {
-        const  pageItem = document.createElement("li");
-        const  pageLink = document.createElement("a");
-        pageLink.textContent = i;
-        pageLink.addEventListener("click", (event) => {
-            event.preventDefault();
-            currentPage = i;
-            displayProducts(currentPage);
-            setupPagination();
-        });
-
-        pageItem.appendChild(pageLink);
-        pagination.appendChild(pageItem);
+function setupPagination(currentPage, totalPages) {
+    let paginationHTML = '';
+    
+    //Nút Previous
+    if(currentPage > 1) {
+        paginationHTML += `<li class="btn-pagination"><a href="#" onclick="changePage(${currentPage - 1});">Trước</a></li>`
+    } else {
+        paginationHTML += `<li class="disabled btn-pagination"><span>Trước</span></li>`;
     }
+    
+    // Trang đầu tiên và dấu ...
+    if(currentPage > 3) {
+        paginationHTML += `<li class="btn-pagination"><a href="#" onclick="changePage(1)">1</a></li>`;
+        paginationHTML += `<li><span>...</span></li>`;
+    }
+    
+    // Cac trang gan trang hien tai
+    for(let i = Math.max(1, currentPage -2); i <= Math.min(currentPage +2, totalPages); i++){
+        if(i === currentPage) {
+            paginationHTML += `<li class="btn-pagination"><span class="active">${i}</span></li>`;
+        } else {
+            paginationHTML += `<li class="btn-pagination"><a href="#" onclick="changePage(${i})">${i}</a></li>`;
+        }
+    }
+    
+    // Dau ... va trang cuoi cung
+    if(currentPage < totalPages -2) {
+        paginationHTML += `<li><span>...</span></li>`;
+        paginationHTML += `<li class="btn-pagination"><a href="#" onclick="changePage(${totalPages})">${totalPages}</a></li>`;
+    }
+    
+    //Nut next
+    if(currentPage < totalPages) {
+        paginationHTML += `<li class="btn-pagination"><a href="#" onclick="changePage(${currentPage + 1})">Next</a></li>`;
+    }
+    else {
+        paginationHTML += `<li class="disabled btn-pagination"><span>Next</span></li>`;
+    }
+    
+    document.getElementById('pagination').innerHTML = paginationHTML;
+}
+
+function changePage(page) {
+    setupPagination(page, totalPages);
+    displayProducts(page);
 }
 
 // khoi dong trang
-displayProducts(currentPage);
-setupPagination();
+setupPagination(1, totalPages);
+displayProducts(1);
