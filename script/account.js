7c6d06dad3b202ab89686396054e325d93e4f6f4
Loan Nguyen <22130148@st.hcmuaf.edu.vn>
2024-11-13 16:22:27 +0700

    Chưa làm xong jQuery thêm/xóa sản phẩm trong lịch sử xem.

--- script/account.js
+++ script/account.js
@@ -13,6 +13,42 @@ $(document).ready(function(){
          return $(".sub_change_pass").css("display", "block");
     })
     //Change name
+    $("#save_info").click(function(){
+        var fname= $("#fname").val();
+        var lname=$("#lname").val();
+        return $("#fullname").text(fname+" "+lname);
+    })
+    //Thêm địa chỉ mới
 
-    //
+    //Thay đổi lịch sử xem sản phẩm
+    $('.add-to_cart').click(function() {
+        // Find and remove the product item
+        $(this).closest('.product-item').remove();
+
+        // Check if there are any remaining products in the same shop
+        var shop = $(this).closest('.shop');
+        if (shop.find('.product-item').length === 0) {
+            // Remove the shop if no products remain
+            shop.remove();
+        }
+    })
+    //Thêm hết vào giỏ hàng.
+    $(".addAll").click(function (){
+        $('.shop').each(function (){
+            $(this).find('.product-item').remove();
+            $(this).remove();
+        })
+    })
+    // xoa san pham
+    $('.delete_btn').click(function() {
+        // Find and remove the product item
+        $(this).closest('.product-item').remove();
+
+        // Check if there are any remaining products in the same shop
+        var shop = $(this).closest('.shop');
+        if (shop.find('.product-item').length === 0) {
+            // Remove the shop if no products remain
+            shop.remove();
+        }
+    })
 })
