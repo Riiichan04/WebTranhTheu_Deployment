2ab6d3b03f6f0fb35dbeb2da350bdf60c7002d5a
Tran-3110 <22130276@st.hcmuaf.edu.vn>
2024-11-16 03:02:12 +0700

    viết file js và tạo các trang html cho các form thêm, xóa, sửa cho quản lý sản phẩm

---
+++ script/admin/product-form.js
@@ -0,0 +1,79 @@
+function changeFormOnProduct(btn) {
+    const formWrapper = document.getElementById('formWrapper');
+    switch (btn) {
+        case 'addProductBtn':
+            formWrapper.innerHTML = `<iframe src="product-form/add-product-form.html" class="w-100 h-100" id="addIframe"></iframe>`;
+            break;
+        case 'readAndEditUserBtn':
+            formWrapper.innerHTML = `<iframe src="product-form/read-edit-product-form.html" class="w-100 h-100" id="readAndEditIframe"></iframe>`;
+            break;
+        case 'deleteBtn':
+            formWrapper.innerHTML = `<iframe src="product-form/delete-popup.html" class="w-100 h-100" id="deleteIframe"></iframe>`;
+            break;
+    }
+}
+
+$(document).ready(function () {
+    $('#addProductBtn').click(function () {
+        changeFormOnProduct(this.id);
+        $('#formWrapper').removeClass('hidden');
+
+        $('#addIframe').on('load', function () {
+            const iframeDoc = $(this).contents();
+
+            // Gán sự kiện vào phần tử bên trong iframe
+            iframeDoc.find('#cancelBtn').on('click', function () {
+                $('#overlay').addClass('hidden');
+                $('#formWrapper').addClass('hidden');
+            });
+
+            iframeDoc.find('#overlay').on('click', function () {
+                $('#overlay').addClass('hidden');
+                $('#formWrapper').addClass('hidden');
+            });
+        });
+    });
+
+    $('.btn-read-edit').click(function () {
+        changeFormOnProduct('readAndEditUserBtn');
+        $('#formWrapper').removeClass('hidden');
+
+        $('#readAndEditIframe').on('load', function () {
+            const iframeDoc = $(this).contents();
+
+            // Gán sự kiện vào phần tử bên trong iframe
+            iframeDoc.find('#cancelBtn').on('click', function () {
+                $('#overlay').addClass('hidden');
+                $('#formWrapper').addClass('hidden');
+            });
+
+            iframeDoc.find('#overlay').on('click', function () {
+                $('#overlay').addClass('hidden');
+                $('#formWrapper').addClass('hidden');
+            });
+        });
+
+    });
+
+    $('.btn-delete').click(function () {
+        changeFormOnProduct('deleteBtn');
+        $('#formWrapper').removeClass('hidden');
+
+        $('#deleteIframe').on('load', function () {
+            const iframeDoc = $(this).contents();
+
+            // Gán sự kiện vào phần tử bên trong iframe
+            iframeDoc.find('#cancelBtn').on('click', function () {
+                $('#overlay').addClass('hidden');
+                $('#formWrapper').addClass('hidden');
+            });
+
+            iframeDoc.find('#overlay').on('click', function () {
+                $('#overlay').addClass('hidden');
+                $('#formWrapper').addClass('hidden');
+            });
+        });
+
+    });
+
+});
