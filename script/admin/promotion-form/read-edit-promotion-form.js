34b04c249a2830dbfb2fb5f10b7f922cf8c8d474
Tran-3110 <22130276@st.hcmuaf.edu.vn>
2024-11-19 01:35:32 +0700

    Viết js cho nút chỉnh sửa form Xem và sửa giảm giá.

--- script/admin/promotion-form/read-edit-promotion-form.js
+++ script/admin/promotion-form/read-edit-promotion-form.js
@@ -11,3 +11,27 @@ $(document).ready(function () {
     $('.dt-search input').css("width", "300px");
     $('table.dataTable th.dt-type-numeric').css("text-align", "center");
 });
+
+$('#editBtn').click(function () {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+    $('#title').text('Chỉnh sửa giảm giá')
+    $('#containerEditBtn').html(`<button class="style-update-btn" id="editBtn" type="submit">Cập nhật</button>`);
+    $('#containerCancelBtn').html(`<button type="button" id="cancelEditBtn" class="style-cancel-btn">Hủy</button>`)
+    $('#cancelEditBtn').click(function () {
+        location.reload();
+    });
+
+    $('#nameDiscount').prop('disabled', false);
+
+    $('#description').prop('disabled', false);
+    
+    $('.percent-discount').prop('disabled', false);
+    
+    $('.amount-limit').prop('disabled', false);
+
+    $('#startDateDiscount').prop('disabled', false);
+    
+    $('#endDateDiscount').prop('disabled', false);
+    
+    $('.edit-hidden').removeClass('d-none');
+});
