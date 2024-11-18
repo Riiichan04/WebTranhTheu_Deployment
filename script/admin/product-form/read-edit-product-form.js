e44c94f81249277f47d3afaa7169b12e174468eb
Tran-3110 <22130276@st.hcmuaf.edu.vn>
2024-11-18 18:28:02 +0700

    Viết js cho nút chỉnh sửa form Xem và sửa sản phẩm.

--- script/admin/product-form/read-edit-product-form.js
+++ script/admin/product-form/read-edit-product-form.js
@@ -11,3 +11,41 @@ $(document).ready(function () {
     $('.dt-search input').css("width", "300px");
     $('table.dataTable th.dt-type-numeric').css("text-align", "center");
 });
+
+$('#editBtn').click(function () {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+    $('#title').text('Chỉnh sửa sản phẩm')
+    $('#containerEditBtn').html(`<button class="style-update-btn" id="editBtn" type="submit">Cập nhật</button>`);
+    $('#containerCancelBtn').html(`<button type="button" id="cancelEditBtn" class="style-cancel-btn">Hủy</button>`)
+    $('#cancelEditBtn').click(function () {
+        location.reload();
+    });
+    
+    $('#nameProduct').prop('disabled', false);
+
+    $('#descriptionProduct').prop('disabled', false);
+
+    $('#price').prop('disabled', false);
+    
+    $('#quantityProduct').prop('disabled', false);
+    
+    $('#widthProduct').prop('disabled', false);
+    
+    $('#heightProduct').prop('disabled', false);
+    
+    $('#material').prop('disabled', false);
+    
+    $('#provider').prop('disabled', false);
+    
+    $('#policy').prop('disabled', false);
+    
+    $('#titleCategory').text('Xóa danh mục (Chọn nhiều)');
+    
+    $('#category').prop('disabled', false);
+    
+    $('.reviewByStar').prop('disabled', false);
+    
+    $('.comment').prop('disabled', false);
+
+    $('.edit-hidden').removeClass('d-none');
+});
