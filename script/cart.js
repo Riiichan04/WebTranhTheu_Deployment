162be8006faed80b8c79dc6b3356cefd38fa3206
Loan Nguyen <22130148@st.hcmuaf.edu.vn>
2024-11-21 11:05:03 +0700

    chưa xét trường hợp giảm số lượng, bỏ sản phẩm.

--- script/cart.js
+++ script/cart.js
@@ -1,15 +1,17 @@
 const MAX_VALUE = 5;
 const MIN_VALUE = 1;
+let total_price
 const formatter = new Intl.NumberFormat('vi-VN', {
     style: 'currency',
     currency: 'VND',
 });
+
 //sum price for pay
-function calculateTotal(){
-    let total_price=0;
+function calculateTotal() {
+    total_price = 0;
     $(".product-checkbox").each(function () {
-        let amount=0;
-        let priceEach=0;
+        let amount = 0;
+        let priceEach = 0;
         $(this).change(function () {
             if ($(this).is(':checked')) {
                 let cart_item = $(this).parents(".cart-item")
@@ -19,14 +21,13 @@ function calculateTotal(){
                 priceEach = parseInt(priceEachElement.replace(/\D/g, ''), 10)
                 console.log(amount)
                 console.log(priceEach)
-                total_price = total_price + (amount*priceEach);
+                total_price = total_price + (amount * priceEach);
                 console.log(total_price)
-                $("#total-price").text(formatter.format(total_price +""))
-            }
-            else{
-                total_price = total_price - (amount*priceEach);
+                $("#total-price").text(formatter.format(total_price + ""))
+            } else {
+                total_price = total_price - (amount * priceEach);
                 console.log(total_price)
-                $("#total-price").text(formatter.format(total_price +""))
+                $("#total-price").text(formatter.format(total_price + ""))
             }
         })
     })
@@ -43,10 +44,24 @@ $(".product-detail__price").each(function () {
 //Choose all
 $("#checkAll").change(function () {
     if ($(this).is(':checked')) {
-        $(".product-checkbox").prop('checked',true);
-        calculateTotal();
-    } else{
+        $(".product-checkbox").prop('checked', true);
+        total_price = 0;
+        $(".product-checkbox").each(function () {
+            let amount = 0;
+            let priceEach = 0;
+            let cart_item = $(this).parents(".cart-item")
+            const amountElement = cart_item.find(".product-detail__amount").prop("innerText")
+            const priceEachElement = cart_item.find(".product-detail__price").prop("innerText")
+            amount = parseInt(amountElement)
+            priceEach = parseInt(priceEachElement.replace(/\D/g, ''), 10)
+            total_price = total_price + (amount * priceEach);
+            console.log(total_price)
+            $("#total-price").text(formatter.format(total_price + ""))
+        })
+    } else {
         $(".product-checkbox").prop('checked', false)
+        total_price=0;
+        $("#total-price").text(formatter.format(total_price+ ""))
     }
 })
 
