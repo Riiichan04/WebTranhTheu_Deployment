8ec9f2bc1068a3088fa20592be792b0efa419e96
Tran-3110 <22130276@st.hcmuaf.edu.vn>
2024-11-25 01:02:48 +0700

    Chỉnh popup các form chính sách

--- script/admin/change-policy-form.js
+++ script/admin/change-policy-form.js
@@ -1,14 +1,53 @@
 function changeFormOnPolicy(btn) {
+    $('#formWrapper').css({
+        'display': 'flex',
+        'justify-content': 'center',
+        'align-items': 'center'
+    });
+    
     const formWrapper = document.getElementById('formWrapper');
     switch (btn) {
         case 'addPolicyBtn':
-            formWrapper.innerHTML = `<iframe src="policy-form/add-policy-form.html" class="w-100 h-100" id="addIframe"></iframe>`;
+            formWrapper.innerHTML = `<iframe src="policy-form/add-policy-form.html" class="form-popup" id="addIframe"></iframe>`;
+            const add_iframe = document.getElementById('addIframe');
+
+            // Lắng nghe sự kiện load để đảm bảo iframe đã tải xong
+            add_iframe.addEventListener('load', function () {
+                const c = add_iframe.contentWindow;
+                if(c.getHeightForm() < screen.height) {
+                    $('#addIframe').css('height', (c.getHeightForm()+2) + 'px');
+                } else {
+                    $('#addIframe').css('height', (screen.height -170) + 'px');
+                }
+            });
             break;
         case 'readAndEditUserBtn':
-            formWrapper.innerHTML = `<iframe src="policy-form/read-edit-policy-form.html" class="w-100 h-100" id="readAndEditIframe"></iframe>`;
+            formWrapper.innerHTML = `<iframe src="policy-form/read-edit-policy-form.html" class="form-popup" id="readAndEditIframe"></iframe>`;
+            const re_iframe = document.getElementById('readAndEditIframe');
+
+            // Lắng nghe sự kiện load để đảm bảo iframe đã tải xong
+            re_iframe.addEventListener('load', function () {
+                const c = re_iframe.contentWindow;
+                if(c.getHeightForm() < screen.height) {
+                    $('#readAndEditIframe').css('height', c.getHeightForm() + 'px');
+                } else {
+                    $('#readAndEditIframe').css('height', screen.height + 'px');
+                }
+            });
             break;
         case 'deleteBtn':
-            formWrapper.innerHTML = `<iframe src="policy-form/delete-popup.html" class="w-100 h-100" id="deleteIframe"></iframe>`;
+            formWrapper.innerHTML = `<iframe src="policy-form/delete-popup.html" class="form-popup" id="deleteIframe"></iframe>`;
+            const del_iframe = document.getElementById('deleteIframe');
+
+            // Lắng nghe sự kiện load để đảm bảo iframe đã tải xong
+            del_iframe.addEventListener('load', function () {
+                const c = del_iframe.contentWindow;
+                if(c.getHeightForm() < screen.height) {
+                    $('#deleteIframe').css('height', c.getHeightForm() + 'px');
+                } else {
+                    $('#deleteIframe').css('height', (screen.height -170) + 'px');
+                }
+            });
             break;
     }
 }
@@ -23,13 +62,7 @@ $('#myTable').DataTable().on('draw', function () {
 
             // Gán sự kiện vào phần tử bên trong iframe
             iframeDoc.find('#cancelBtn').on('click', function () {
-                $('#overlay').addClass('hidden');
-                $('#formWrapper').addClass('hidden');
-            });
-
-            iframeDoc.find('#overlay').on('click', function () {
-                $('#overlay').addClass('hidden');
-                $('#formWrapper').addClass('hidden');
+                hiddenOverlay();
             });
         });
     });
@@ -43,13 +76,7 @@ $('#myTable').DataTable().on('draw', function () {
 
             // Gán sự kiện vào phần tử bên trong iframe
             iframeDoc.find('#cancelBtn').on('click', function () {
-                $('#overlay').addClass('hidden');
-                $('#formWrapper').addClass('hidden');
-            });
-
-            iframeDoc.find('#overlay').on('click', function () {
-                $('#overlay').addClass('hidden');
-                $('#formWrapper').addClass('hidden');
+                hiddenOverlay();
             });
         });
 
@@ -64,16 +91,22 @@ $('#myTable').DataTable().on('draw', function () {
 
             // Gán sự kiện vào phần tử bên trong iframe
             iframeDoc.find('#cancelBtn').on('click', function () {
-                $('#overlay').addClass('hidden');
-                $('#formWrapper').addClass('hidden');
-            });
-
-            iframeDoc.find('#overlay').on('click', function () {
-                $('#overlay').addClass('hidden');
-                $('#formWrapper').addClass('hidden');
+                hiddenOverlay();
             });
         });
 
     });
 
 });
+
+$('#formWrapper').click(function () {
+    hiddenOverlay();
+});
+
+function hiddenOverlay() {
+    $('#formWrapper').css({
+        'display': 'none',
+        'justify-content': 'none',
+        'align-items': 'none'
+    });
+}
