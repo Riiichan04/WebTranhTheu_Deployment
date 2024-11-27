821e4913add301ab993e6483a0259ba7165f7d3e
Riichan04 <22130274@st.hcmuaf.edu.vn>
2024-11-27 21:09:26 +0700

    Thêm sự kiện cho các button trong trang

--- script/category.js
+++ script/category.js
@@ -1,10 +1,20 @@
 $(".category-element").click(function () {
     $(".category-element").removeClass("category-active");
     $(this).addClass("category-active");
+    $("#category-title").text($(this).text());
 })
 
 $(".card").click(function () {
     window.location = "../page/product.html"
+})
+
+$("#remove-filter").click(function () {
+    $("input[type=text]").val("");
+    $("input[type=radio]").prop('checked', false);
+})
+
+$("#filter-btn").click(function () {
+    location.reload();
 })
 
 //Sẽ làm sau khi merge file data.json
