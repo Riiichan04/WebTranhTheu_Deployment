b79668bbe95a9d2b1bb4e5564b914fff0516d1e3
Tran-3110 <22130276@st.hcmuaf.edu.vn>
2024-12-12 21:07:03 +0700

    Chỉnh sửa html các nút thêm, xóa trong form thêm, sửa người dùng và form giảm giá, thêm trường cho form sửa giảm giá

--- script/admin/promotion-form/read-edit-promotion-form.js
+++ script/admin/promotion-form/read-edit-promotion-form.js
@@ -1,5 +1,38 @@
+function addProduct() {
+    if (document.querySelectorAll('.discount-row').length < 1) {
+        const discountRow = document.createElement('div');
+        discountRow.className = "form-group discount-row row mb-2";
+        discountRow.innerHTML = `<div class="col pe-0">
+                            <label class="style-label">Mã sản phẩm<span class="text-danger"> * </span></label>
+                            <input type="text" class="style-input" placeholder="Nhập mã sản phẩm" required>
+                        </div>
+                        <div class="col pe-0">
+                            <label class="style-label">Phần trăm giảm (%)<span class="text-danger"> * </span></label>
+                            <input type="number" class="w-100 style-input" min="1" max="100" placeholder="Nhập % giảm" required>
+                        </div>
+                        <div class="col">
+                            <label class="style-label">Số lượng giới hạn</label>
+                            <input type="number" class="w-100 style-input" min="1" placeholder="Nhập số lượng">
+                        </div>
+                        <div class="col-1 p-0 mt-3">
+                            <i class="fa-solid fa-trash icon-del" onclick="removeProduct(this)"></i>
+                        </div>`;
+        document.getElementById('discounts').appendChild(discountRow);
+    } else {
+        const discountRow = document.querySelector('.discount-row');
+        const newRow = discountRow.cloneNode(true);
+        newRow.querySelectorAll('input').forEach(input => input.value = '');
+        document.getElementById('discounts').appendChild(newRow);
+    }
+}
+
+function removeProduct(button) {
+    const row = button.parentElement.parentElement;
+    row.remove();
+}
+
 $(document).ready(function () {
-    $('#myTable').DataTable( {
+    $('#myTable').DataTable({
         destroy: true,
         scrollY: "200px"
     }); // Khởi tạo DataTable
@@ -13,7 +46,7 @@ $(document).ready(function () {
 });
 
 $('#editBtn').click(function () {
-    window.scrollTo({ top: 0, behavior: 'smooth' });
+    window.scrollTo({top: 0, behavior: 'smooth'});
     $('#title').text('Chỉnh sửa giảm giá')
     $('#containerEditBtn').html(`<button class="style-update-btn" id="editBtn" type="submit">Cập nhật</button>`);
     $('#containerCancelBtn').html(`<button type="button" id="cancelEditBtn" class="style-cancel-btn">Hủy</button>`)
@@ -24,14 +57,14 @@ $('#editBtn').click(function () {
     $('#nameDiscount').prop('disabled', false);
 
     $('#description').prop('disabled', false);
-    
+
     $('.percent-discount').prop('disabled', false);
-    
+
     $('.amount-limit').prop('disabled', false);
 
     $('#startDateDiscount').prop('disabled', false);
-    
+
     $('#endDateDiscount').prop('disabled', false);
-    
+
     $('.edit-hidden').removeClass('d-none');
 });
