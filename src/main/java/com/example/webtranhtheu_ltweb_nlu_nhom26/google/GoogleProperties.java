d7991f4530765314cb1491ac76f9b4e2316809a6
Riichan04 <22130274@st.hcmuaf.edu.vn>
2024-12-18 22:18:35 +0700

    Fix lỗi url trên admin.js

--- src/main/java/com/example/webtranhtheu_ltweb_nlu_nhom26/google/GoogleProperties.java
+++
@@ -1,4 +0,0 @@
-package com.example.webtranhtheu_ltweb_nlu_nhom26.google;
-
-public class GoogleProperties {
-}
--- src/main/webapp/template/script/admin/admin.js
+++ src/main/webapp/template/script/admin/admin.js
@@ -2,31 +2,31 @@ function loadPage(page) {
     let content = document.getElementById("content")
     switch (page) {
         case "dashboard":
-            content.innerHTML = "<iframe src='../../admin/dashboard.html' class='w-100 vh-100'></iframe>";
+            content.innerHTML = "<iframe src='/src/main/webapp/template/admin/dashboard.html' class='w-100 vh-100'></iframe>";
             break;
         case "product":
-            content.innerHTML = "<iframe src='../../admin/manage-product.html' class='w-100 vh-100'></iframe>";
+            content.innerHTML = "<iframe src='/src/main/webapp/template/admin/manage-product.html' class='w-100 vh-100'></iframe>";
             break;
         case "user":
-            content.innerHTML = "<iframe src='../../admin/manage-user.html' class='w-100 vh-100'></iframe>";
+            content.innerHTML = "<iframe src='/src/main/webapp/template/admin/manage-user.html' class='w-100 vh-100'></iframe>";
             break;
         case "promotion":
-            content.innerHTML = "<iframe src='../../admin/manage-promotion.html' class='w-100 vh-100'></iframe>";
+            content.innerHTML = "<iframe src='/src/main/webapp/template/admin/manage-promotion.html' class='w-100 vh-100'></iframe>";
             break;
         case "category":
-            content.innerHTML = "<iframe src='../../admin/manage-category.html' class='w-100 vh-100'></iframe>";
+            content.innerHTML = "<iframe src='/src/main/webapp/template/admin/manage-category.html' class='w-100 vh-100'></iframe>";
             break;
         case "topic":
-            content.innerHTML = "<iframe src='../../admin/manage-topic.html' class='w-100 vh-100'></iframe>";
+            content.innerHTML = "<iframe src='/src/main/webapp/template/admin/manage-topic.html' class='w-100 vh-100'></iframe>";
             break;
         case "order":
-            content.innerHTML = "<iframe src='../../admin/manage-order.html' class='w-100 vh-100'></iframe>";
+            content.innerHTML = "<iframe src='/src/main/webapp/template/admin/manage-order.html' class='w-100 vh-100'></iframe>";
             break;
         case "policy":
-            content.innerHTML = "<iframe src='../../admin/manage-policy.html' class='w-100 vh-100'></iframe>";
+            content.innerHTML = "<iframe src='/src/main/webapp/template/admin/manage-policy.html' class='w-100 vh-100'></iframe>";
             break;
         case "setting":
-            content.innerHTML = "<iframe src='../../admin/setting-user.html' class='w-100 vh-100'></iframe>";
+            content.innerHTML = "<iframe src='/src/main/webapp/template/admin/setting-user.html' class='w-100 vh-100'></iframe>";
             break;
     }
 }
