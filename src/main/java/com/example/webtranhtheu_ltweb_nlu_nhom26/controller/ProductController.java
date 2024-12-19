5708f2f2e2d283104c96af0c9e0782e65c6683e8
Riichan04 <22130274@st.hcmuaf.edu.vn>
2024-12-19 22:19:24 +0700

    Deploy cơ bản trang sản phẩm lên controller

---
+++ src/main/java/com/example/webtranhtheu_ltweb_nlu_nhom26/controller/ProductController.java
@@ -0,0 +1,22 @@
+package com.example.webtranhtheu_ltweb_nlu_nhom26.controller;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+import java.io.IOException;
+
+@WebServlet(name = "ProductController", value = "/product")
+public class ProductController extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setCharacterEncoding("utf-8");
+        response.setCharacterEncoding("utf-8");
+        request.getRequestDispatcher("/layout/product.jsp").forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
