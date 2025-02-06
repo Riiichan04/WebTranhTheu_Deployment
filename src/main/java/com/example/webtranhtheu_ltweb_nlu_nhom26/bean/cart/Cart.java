729a5450ec8f31a93a56fb5133e5050d42a8ccb9
Loan Nguyen <22130148@st.hcmuaf.edu.vn>
2025-02-06 11:20:06 +0700

    Xét trường hợp không có Discount thì không hiển thị

--- src/main/java/com/example/webtranhtheu_ltweb_nlu_nhom26/bean/cart/Cart.java
+++ src/main/java/com/example/webtranhtheu_ltweb_nlu_nhom26/bean/cart/Cart.java
@@ -123,7 +123,7 @@ public class Cart implements Serializable {
 
 
     public Discount getMaxDiscount() {
-        return this.discountList.stream().max(Comparator.comparingDouble(Discount::getValue)).orElse(this.discountList.get(0));
+        return this.discountList.stream().max(Comparator.comparingDouble(Discount::getValue)).orElse(null);
     }
 
     public Discount getSelectedDiscount(int discountId) {
