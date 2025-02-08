d1fcbf30f5c8c433d63ba2a898d747bd45ae5027
Riichan04 <22130274@st.hcmuaf.edu.vn>
2025-02-08 15:26:46 +0700

    Debug phần listTopic

--- src/main/java/com/example/webtranhtheu_ltweb_nlu_nhom26/controller/category/CategoryProductFilter.java
+++ src/main/java/com/example/webtranhtheu_ltweb_nlu_nhom26/controller/category/CategoryProductFilter.java
@@ -43,16 +43,20 @@ public class CategoryProductFilter extends HttpServlet {
             int amount = Integer.parseInt(requestAmount);
             int page = Integer.parseInt(requestPage);
 
+            System.out.println("-----");
             System.out.println("Category Filter Log:");
+            System.out.println(requestListTopicId);
             System.out.println("Name: " + productName);
             System.out.println("PatternName: " + patternName);
             System.out.println("ProviderName: " + providerName);
-            System.out.println("ListTopic: " + Arrays.toString(listTopicId.toArray()));
+            System.out.println("ListTopic: " + listTopicId);
             System.out.println("Rating: " + rating);
             System.out.println("From Price: " +fromPrice);
             System.out.println("To Price: " + toPrice);
             System.out.println("Amount: " + amount);
             System.out.println("Page: " + page);
+            System.out.println("-----");
+            System.out.println();
 
             if (page <= 0 || amount <= 0 || fromPrice > toPrice || rating < 0 || rating > 5) {
                 ControllerUtil.sendAjaxResultFalse(response, jsonResult, null);
